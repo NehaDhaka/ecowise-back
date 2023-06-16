ff1dc5463f44f6461f725ff2b2ec2e1c52398c48
Neha Dhaka <nehadhaka43@gmail.com>
2023-06-16 10:30:34 -0400

    add mock data to seed file

--- controllers/productController.js
+++ controllers/productController.js
@@ -6,12 +6,11 @@ const index = (_req, res) => {
       res.status(200).json(data);
     })
     .catch((err) => res.status(400).send(`Error retrieving products: ${err}`));
-  console.log("Displaying all products");
 };
 
-const findOne = (req, res) => {
+const findOne = (_req, res) => {
   knex("products")
-    .where({ id: req.params.id })
+    .where({ id: _req.params.id })
     .then((productFound) => {
       if (productFound.length === 0) {
         return res
@@ -25,7 +24,7 @@ const findOne = (req, res) => {
     })
     .catch(() => {
       res.status(500).json({
-        message: `Unable to retrieve product data for product with ID: ${req.params.id}`,
+        message: `Unable to retrieve product data for product with ID: ${_req.params.id}`,
       });
     });
 };
--- seeds/products.js
+++ seeds/products.js
@@ -7,8 +7,8 @@ exports.seed = async function (knex) {
   await knex("products").del();
   await knex("products").insert([
     {
-      name: "Hanes t-shirt",
-      img: "test-img",
+      name: "Organic Black Tee",
+      img: `http://localhost:5050/images/product-1.png`,
       price_range: "$12.99 - 14.99",
       description:
         "100% Cotton. Made with sustainably sourced USA grown cotton. Classic Fit",
@@ -18,7 +18,67 @@ exports.seed = async function (knex) {
     },
     {
       name: "Fruit of the Loom",
-      img: "test-img",
+      img: `http://localhost:5050/images/product-2.png`,
+      price_range: "$14.99 - 16.99",
+      description:
+        "100% Cotton. Made with sustainably sourced USA grown cotton. Classic Fit",
+      eco_rating: 95,
+      type: "Black t-shirt",
+      category: "Apparel",
+    },
+    {
+      name: "Fruit of the Loom",
+      img: `http://localhost:5050/images/product-3.png`,
+      price_range: "$14.99 - 16.99",
+      description:
+        "100% Cotton. Made with sustainably sourced USA grown cotton. Classic Fit",
+      eco_rating: 95,
+      type: "Black t-shirt",
+      category: "Apparel",
+    },
+    {
+      name: "Fruit of the Loom",
+      img: `http://localhost:5050/images/product-4.png`,
+      price_range: "$14.99 - 16.99",
+      description:
+        "100% Cotton. Made with sustainably sourced USA grown cotton. Classic Fit",
+      eco_rating: 95,
+      type: "Black t-shirt",
+      category: "Apparel",
+    },
+    {
+      name: "Fruit of the Loom",
+      img: `http://localhost:5050/images/product-5.png`,
+      price_range: "$14.99 - 16.99",
+      description:
+        "100% Cotton. Made with sustainably sourced USA grown cotton. Classic Fit",
+      eco_rating: 95,
+      type: "Black t-shirt",
+      category: "Apparel",
+    },
+    {
+      name: "Fruit of the Loom",
+      img: `http://localhost:5050/images/product-6.png`,
+      price_range: "$14.99 - 16.99",
+      description:
+        "100% Cotton. Made with sustainably sourced USA grown cotton. Classic Fit",
+      eco_rating: 95,
+      type: "Black t-shirt",
+      category: "Apparel",
+    },
+    {
+      name: "Fruit of the Loom",
+      img: `http://localhost:5050/images/product-7.png`,
+      price_range: "$14.99 - 16.99",
+      description:
+        "100% Cotton. Made with sustainably sourced USA grown cotton. Classic Fit",
+      eco_rating: 95,
+      type: "Black t-shirt",
+      category: "Apparel",
+    },
+    {
+      name: "Fruit of the Loom",
+      img: `http://localhost:5050/images/product-8.png`,
       price_range: "$14.99 - 16.99",
       description:
         "100% Cotton. Made with sustainably sourced USA grown cotton. Classic Fit",
